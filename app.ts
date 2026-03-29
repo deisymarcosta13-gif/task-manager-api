cef5732130414512241d87b656c2aee775269ee9
Deisy Mariana Acosta Correa <deisymarcosta13@gmail.com>
2026-03-29 19:25:01 +0200

    chore: inicialización del proyecto con TypeScript y estructura base

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando ");
+});
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
